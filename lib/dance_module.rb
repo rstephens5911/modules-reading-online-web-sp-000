80542ceddc33bdc4bc63ec8315bff4be3f89605a
Ronald Stephens <rstephens@bluemorpho.cc>
2019-08-13 20:08:47 +0000

    Done.

---
+++ lib/dance_module.rb
@@ -0,0 +1,19 @@
+module Dance
+
+  def twirl
+    "Im twirling!"
+  end
+
+  def jump
+    "I'm jumping!"
+  end
+
+  def pirouette
+    "I'm doing a pirouette"
+  end
+
+  def take_a_bow
+    "Thank you, thank you. I was a pleasure to dance for you all."
+  end
+
+end
